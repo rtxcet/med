cf3252bd7ada9a43c9a73097bd9ec4bb95d6f948
rtxcet <cetinrtx@gmail.com>
2025-08-05 08:49:49 +0300

    Finish Slider

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -8,6 +8,7 @@ namespace med
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<Slider> Sliders { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
---
+++ Components/Default/BannerViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using med.Models;
+
+namespace med.Components
+{
+    public class BannerViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public BannerViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var sliders = await _context.Sliders.OrderBy(s => s.Id).ToListAsync();
+            return View(sliders);
+        }
+    }
+}
