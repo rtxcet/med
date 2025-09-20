91df52428cb47e8662f5ba6d6147e6b3dc9b25e8
rtxcet <cetinrtx@gmail.com>
2025-09-21 00:30:47 +0300

    Finish

---
+++ Components/Default/CategoryViewComponent .cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using med.Models;
+
+namespace med.Components
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Son eklenen 9 kategori (Id'ye göre sıralı)
+            var categories = await _context.Categories
+                .OrderByDescending(c => c.Id)
+                .Take(9)
+                .ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
